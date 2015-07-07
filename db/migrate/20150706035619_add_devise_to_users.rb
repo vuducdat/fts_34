f42670e8e44ac16b65538ad375a6a488e262e455
thanhnga67 <thanhngabk1@gmail.com>
2015-07-07 14:40:49 +0700

    sign in and sign out

--- db/migrate/20150706035619_add_devise_to_users.rb
+++ db/migrate/20150706035619_add_devise_to_users.rb
@@ -1,14 +1,11 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
-      ## Database authenticatable
       t.string :encrypted_password, null: false, default: ""
 
-      ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
-      ## Rememberable
       t.datetime :remember_created_at
 
       ## Trackable
